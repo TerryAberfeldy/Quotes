679874c68b39fe16aaf0818596bb0833965b994a
Terry <tmalden69@rogers.com>
2022-04-04 14:12:05 -0400

    Initial Commit

---
+++ Lesson 16 module 2 challenge/Lesson_16_module_2_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_16_module_2_challengeApp.swift
+//  Lesson 16 module 2 challenge
+//
+//  Created by Terry Malden on 2022-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_16_module_2_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
